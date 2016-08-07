2f36260401216dfdc1822b35e45125561d2ce5ab
davidlp <davidlp@davidlp-All-Series>
2016-08-07 14:11:37 +0200

    PRJ: increase version 0.0.1 -> 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
 
-version = '0.0.1'
+version = '0.0.2'
 author = 'David-Leon Pohl'
 author_email = 'David-Leon.Pohl@rub.de'
 
