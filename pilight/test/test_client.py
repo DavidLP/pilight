de0ff1f14447d883e906e7c959e0e1d2ff2228f7
DavidLP <david-leon.pohl@rub.de>
2016-08-11 14:35:32 +0200

    ENH: check for pilight-daemon received data, new test case

--- pilight/test/test_client.py
+++ pilight/test/test_client.py
@@ -5,7 +5,7 @@ Connects to a simulation of a pilight-daemon.
 
 import unittest
 import time
-from mock import patch
+from mock import patch, call
 
 from pilight import pilight
 from pilight.test import pilight_daemon
@@ -77,8 +77,37 @@ class TestClient(unittest.TestCase):
             pilight_client = pilight.Client(host=pilight_daemon.HOST, port=pilight_daemon.PORT)
             pilight_client.set_callback(_callback)
             pilight_client.start()
-            time.sleep(1)
-
+            time.sleep(2)
         pilight_client.stop()
-        mock.assert_called_with(pilight_daemon.FAKE_DATA)
 
+        # Check if callback was called several times with expected data
+        mock.assert_has_calls([call(pilight_daemon.FAKE_DATA)] * 10)
+
+    @patch('pilight.test.test_client._callback')
+    def test_no_receive_filter(self, mock):
+        """Test for successfull code received."""
+        with pilight_daemon.PilightDaemon(send_codes=True):
+            pilight_client = pilight.Client(host=pilight_daemon.HOST, port=pilight_daemon.PORT, veto_repeats=False)
+            pilight_client.set_callback(_callback)
+            pilight_client.start()
+            time.sleep(2)
+        pilight_client.stop()
+
+        # Check if calls with repeats != 1 have been made
+        calls = []
+        for i in range(10):
+            fake_data = pilight_daemon.FAKE_DATA.copy()
+            fake_data['repeats'] = i + 1
+            calls.append(call(fake_data))
+
+        mock.assert_has_calls(calls)
+
+    def test_invalid_identification(self):
+        """Send an invalid identification and check for connection failure."""
+        recv_ident = {
+            "action": "invalid"
+        }
+
+        with self.assertRaises(IOError):
+            with pilight_daemon.PilightDaemon(send_codes=True):
+                pilight.Client(host=pilight_daemon.HOST, port=pilight_daemon.PORT, recv_ident=recv_ident)
