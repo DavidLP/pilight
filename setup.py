c4053937377c0ed99715fd69fec8b7c315080e80
David-Leon Pohl <pohl@physik.uni-bonn.de>
2016-09-13 08:31:00 +0200

    PRJ version 0.0.2->0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages  # This setup relies on setuptools since distutils is insufficient and badly hacked code
 
-version = '0.0.2'
+version = '0.1.0'
 author = 'David-Leon Pohl'
 author_email = 'David-Leon.Pohl@rub.de'
 
