160d4e43cad1b7c091ba0f2ac4ac6e8fce83ce19
davidlp <pohl@physik.uni-bonn.de>
2016-10-19 11:48:12 +0200

    BUG: determine version without exception, addressing #1

--- pilight/__init__.py
+++ pilight/__init__.py
@@ -1,3 +1,12 @@
 # http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
-from pkg_resources import get_distribution
-__version__ = get_distribution('pilight').version
+from pkg_resources import get_distribution, DistributionNotFound
+
+__project__ = 'pilight'
+__version__ = None  # required for initial installation
+
+try:
+    __version__ = get_distribution(__project__).version
+except DistributionNotFound:
+    VERSION = __project__ + '-' + '(local)'
+else:
+    VERSION = __project__ + '-' + __version__
